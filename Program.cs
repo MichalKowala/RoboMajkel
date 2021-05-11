02d66dcadcb4e3fd7e310ad5f47be456369fdceb
Michał Kowala <michalkowala111@gmail.com>
2021-05-11 16:47:21 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RoboMajkel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
